bfbaa74192d399fbbfd3aeeeb421a38cb4868c84
Avishek kait <avishekkait@gmail.com>
2025-10-15 16:58:52 +0600

    A. Sum

---
+++ 112A.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main( ) {
+int t;
+scanf("%d",&t);
+while(t--){
+int a,b,c;
+scanf("%d %d %d",&a,&b,&c);
+if(a==c+b||b==c+a||c==a+b) 
+printf("YES\n");
+else
+printf("NO\n");
+
+}
+
+
+return 0;
+}
