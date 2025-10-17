e33ccf2f46fc29aec464317872f6d6a506a0cdaa
Avishek kait <avishekkait@gmail.com>
2025-10-18 00:20:20 +0600

    A. Translation

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    char str1[100];
+
+    int a=0;
+    scanf("%s", str);
+        scanf("%s", str1);
+
+
+    int t = strlen(str);
+    for (int i = 0; i < t; i++) {
+      if(str[i]==str1[t-i-1])
+     a = 1;
+            break;
+    }
+    if(a==0)
+    printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+}
