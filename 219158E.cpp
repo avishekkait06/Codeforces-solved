0ad85affc7cef26457d5484d5e0e56dea9b108eb
Avishek kait <avishekkait@gmail.com>
2025-10-23 23:08:17 +0600

    E. Area of a Circle

---
+++ 219158E.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>  // for setprecision
+
+using namespace std;
+
+int main() {
+    double R;
+    cin >> R;
+
+    double pi = 3.141592653;
+    double area = pi * R * R;
+
+    cout << fixed << setprecision(9) << area << endl;
+
+    return 0;
+}
