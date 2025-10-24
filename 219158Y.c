b74bd6ccb4b3966ad2a2cd928827d69b2a63541e
Avishek kait <avishekkait@gmail.com>
2025-10-24 14:09:44 +0600

    Y - The last 2 digits

---
+++ 219158Y.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    long long a, b, c, d;
+    scanf("%lld %lld %lld %lld", &a, &b, &c, &d);
+
+    long long result = a % 100;
+    result = (result * (b % 100)) % 100;
+    result = (result * (c % 100)) % 100;
+    result = (result * (d % 100)) % 100;
+
+    printf("%02lld\n", result);
+    return 0;
+}
+
